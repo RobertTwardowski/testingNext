9d32052a0774cb8eab7cccfcbe5c04302e0bba8f
Robert Twardowski <twardowski.robert1908@gmail.com>
2023-07-22 15:04:12 +0200

    testing

--- src/app/new-meetup/page.js
+++ src/app/new-meetup/page.js
@@ -1,25 +1,14 @@
-import MeetupList from "../components/meetups/MeetupList"
+import NewMeetupForm from "../components/meetups/NewMeetupForm"
 
 
-const DUMMY_MEETUPS =[
-    {id: 'm1',
-     title: 'A First Meetup',
-     image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/2008-09-28_Loreleia_postii_%28Fr%29_Redhead%2CMoncalvo%2CVilgalys_%26_Lutzoni_47564_crop.jpg',
-     adress: 'some adress 5, 12133 Lodz'
-},
-{id: 'm2',
-title: 'A First Meetup',
-image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/2008-09-28_Loreleia_postii_%28Fr%29_Redhead%2CMoncalvo%2CVilgalys_%26_Lutzoni_47564_crop.jpg',
-adress: 'some adress 5, 12133 Lodz'
-},
-{id: 'm3',
-title: 'A First Meetup',
-image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/2008-09-28_Loreleia_postii_%28Fr%29_Redhead%2CMoncalvo%2CVilgalys_%26_Lutzoni_47564_crop.jpg',
-adress: 'some adress 5, 12133 Lodz'
+function NewMeetupPage()
+
+{
+    const addMeetupHandler = (enteredMeetupData) => {
+
+        console.log(enteredMeetupData);
 }
-]
-
-
-export default function HomePage(){
-    return(<MeetupList meetups={DUMMY_MEETUPS}></MeetupList>)
+    return <NewMeetupForm onAdMeetup={addMeetupHandler}/>
 }
+
+export default NewMeetupPage
